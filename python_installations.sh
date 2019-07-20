3aff841d162d3e719868514b3f4199b02e7a7565
Nitin Jain <nitin@alumni.princeton.edu>
2019-07-19 21:01:23 -0700

    help on python installation

---
+++ python_installations.sh
@@ -0,0 +1,9 @@
+sudo apt install python3
+sudo apt install python3-pip
+pip install --upgrade --user numpy
+pip install --upgrade --user scipy
+pip install --upgrade --user pandas
+pip install --upgrade --user matplotlib
+pip install --upgrade --user ipython
+
+
